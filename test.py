9987e2a537ff90336f7c325a05f98e09b2d30e4d
ProWily <williamyi@outlook.sa>
2021-10-25 23:02:13 +0300

    json

--- test.py
+++ test.py
@@ -1,23 +1,44 @@
-message: str = "$add $#Saitama#$ what ever thing you need"
-command = "$add "
+import json, os
 
-print(message[:5])
+PATH: str = os.getcwd()
+data = json.loads(
+    str(open(f"/home/wily/gitrepo/tele_bot/groups/-693319728/logs/log.json").read())
+)
+print(data)
 
 
-def split_info(command, message):
+new_data = {
+    "username": "soe",
+    "user_id": "asd1231",
+    "message": "awdqwerhlwed",
+    "message_type": "command",
+    "discription": "aesjhduajk",
+    "time": "",
+}
+data.update(new_data)
+print(data)
 
-    name = ""
-    for i in range(len(message)):
-        if message[i] == "$" and message[i + 1] == "#":
-            index = i + 2
-            while True:
-                if message[index] == "#" and message[index + 1] == "$":
-                    break
-                name += message[index]
-                index += 1
-            break
-    print(name)
 
-    info = message.replace(command, "").replace(f"$#{name}#$", "")
+# message: str = "$add $#Saitama#$ what ever thing you need"
+# command = "$add "
 
-    print(info)
+# print(message[:5])
+
+
+# def split_info(command, message):
+
+#     name = ""
+#     for i in range(len(message)):
+#         if message[i] == "$" and message[i + 1] == "#":
+#             index = i + 2
+#             while True:
+#                 if message[index] == "#" and message[index + 1] == "$":
+#                     break
+#                 name += message[index]
+#                 index += 1
+#             break
+#     print(name)
+
+#     info = message.replace(command, "").replace(f"$#{name}#$", "")
+
+#     print(info)
--- text_respond.py
+++ text_respond.py
@@ -1,18 +1,23 @@
-import telegram, logging, requests
+import telegram, logging, json
 from datetime import datetime
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 import os
 
 # https://api.telegram.org/bot/getupdates
 # context.bot.send_message(chat_id=update.effective_chat.id, text=update.message.text)
+
 TOKEN: str = ""
-bot = telegram.Bot(token=TOKEN)
-
-logging.basicConfig(
-    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
-)
-
+BOT = telegram.Bot(token=TOKEN)
 PATH: str = os.getcwd()
+
+json = {
+    "username": "soe",
+    "user_id": "asd1231",
+    "message": "awdqwerhlwed",
+    "message_type": "command",
+    "discription": "aesjhduajk",
+    "time": "",
+}
 
 
 def send_msg_group(massege):
@@ -32,10 +37,29 @@ def start(update, context):
 
 
 def echo(update, context):
-    time_now = datetime.now()
-    curr_data = time_now.strftime("%d/%m/%Y")
-    curr_time = time_now.strftime("%H:%M:%S")
     context.bot.send_message(chat_id=update.effective_chat.id, text="Wait Please")
+    # giteing time
+    time_now = datetime.now()
+    curr_date = time_now.strftime("%d/%m/%Y")
+    curr_time = time_now.strftime("%H:%M:%S")
+
+    chat_id = update.effective_chat.id
+    create_dir(chat_id)
+    print(PATH + f"/groups/{chat_id}/logs/log.json")
+    with open(PATH + f"/groups/{chat_id}/logs/log.json", "r") as log_file:
+        print(log_file.read())
+    new_data = {
+        "username": update.message.from_user.username,
+        "user_id": update.message.from_user.id,
+        "message": update.message.text,
+        "time": f"[{curr_date} - {curr_time}]",
+    }
+    log_data = json.loads(new_data)
+    print(log_data)
+
+    log_data.update(new_data)
+
+    print(log_data)
     print(f"here>>{update.message.from_user}")
 
     command: str = update.message.text.lower()
@@ -55,7 +79,7 @@ def echo(update, context):
         finally:
             file.close()
             return
-    elif update.message.from_user.id in get_admin_ids(bot, update.message.chat_id):
+    elif update.message.from_user.id in get_admin_ids(BOT, update.message.chat_id):
         if "/$add " in command[:6]:
             print("data")
 
@@ -64,7 +88,7 @@ def echo(update, context):
             file = open(f"data/{update.effective_chat.id}.{data[0]}", "w")
             file.write(
                 data[1]
-                + f"\n-\n-\n-\n-\n-\nLast Update: \n[{curr_data}]\n[{curr_time}]\nBy: [@{update.message.from_user.username} - {update.message.chat_id}] "
+                + f"\n-\n-\n-\n-\n-\nLast Update: \n[{curr_date}]\n[{curr_time}]\nBy: [@{update.message.from_user.username} - {update.message.chat_id}] "
             )
             print(f"admin : {update.message.from_user.id} ")
             return
@@ -98,7 +122,6 @@ def echo(update, context):
 
 
 def split_info(command, message):
-
     name = ""
     for i in range(len(message)):
         if message[i] == "$" and message[i + 1] == "#":
@@ -109,21 +132,42 @@ def split_info(command, message):
                 name += message[index]
                 index += 1
             break
-    print(name)
-
     info = message.replace(command, "").replace(f"$#{name}#$", "")
     return [name, info]
 
 
-updater = Updater(token=TOKEN, use_context=True)
-dispatcher = updater.dispatcher
+def main():
 
-start_handler = CommandHandler("start", start)
-dispatcher.add_handler(start_handler)
+    logging.basicConfig(
+        format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+        level=logging.INFO,
+    )
 
-echo_handler = MessageHandler(Filters.text & (~Filters.command), echo)
-dispatcher.add_handler(echo_handler)
+    create_dir()
+
+    updater = Updater(token=TOKEN, use_context=True)
+    dispatcher = updater.dispatcher
+
+    start_handler = CommandHandler("start", start)
+    dispatcher.add_handler(start_handler)
+
+    echo_handler = MessageHandler(Filters.text & (~Filters.command), echo)
+    dispatcher.add_handler(echo_handler)
+
+    print(BOT.get_me())
+    updater.start_polling()
 
 
-print(bot.get_me())
-updater.start_polling()
+def create_dir(groub_id=None):
+    if not os.path.isdir(PATH + "/groups"):
+        os.mkdir(PATH + "/groups")
+    if groub_id is not None:
+        if not os.path.isdir(PATH + f"/groups/{groub_id}"):
+            os.mkdir(PATH + f"/groups/{groub_id}")
+            os.mkdir(PATH + f"/groups/{groub_id}/logs")
+            os.mkdir(PATH + f"/groups/{groub_id}/data")
+            open(PATH + f"/groups/{groub_id}/logs/log.json", "w").close()
+
+
+if __name__ == "__main__":
+    main()
